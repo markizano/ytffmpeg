736a0981058e4f93ae929e5bdc1922ecfd8846a6
Markizano Draconus <markizano@markizano.net>
2022-09-07 10:13:41 -0500

    new scripts written to help with calculations. update to htmlize script.

---
+++ bin/t2s.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import os, sys
+
+fps = float( os.environ.get('FPS', '29.45') )
+
+def t2s(h=0,m=0,s=0,f=0):
+  return round( (h * 24) + (m * 60) + s + (f / fps), 3 )
+
+print( t2s(*[ int(x) for x in sys.argv[1:] ]) )
