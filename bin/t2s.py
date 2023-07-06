5350150548a3b5055c65b954564e3550fa2834f9
Markizano Draconus <markizano@markizano.net>
2023-07-06 08:32:17 -0500

    minor fix in t2s when I actually used hours.

--- bin/t2s.py
+++ bin/t2s.py
@@ -5,6 +5,6 @@ import os, sys
 fps = float( os.environ.get('FPS', '29.45') )
 
 def t2s(h=0,m=0,s=0,f=0):
-  return round( (h * 24) + (m * 60) + s + (f / fps), 3 )
+  return round( (h * 3600) + (m * 60) + s + (f / fps), 3 )
 
 print( t2s(*[ round(float(x), 3) for x in sys.argv[1:] ]) )
