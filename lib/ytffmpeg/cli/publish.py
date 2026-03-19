81e753df4f62cbff3733e5e576fd073f3f8d069c
Markizano Draconus <markizano@markizano.net>
2026-03-19 10:32:18 -0500

    changing the entrypoint to publish to match other patterns.

--- lib/ytffmpeg/cli/publish.py
+++ lib/ytffmpeg/cli/publish.py
@@ -10,59 +10,42 @@ will be available via the API's.
 import os
 from fabric import Connection
 
-from kizano import getLogger
+from ytffmpeg import getLogger, utils
 log = getLogger(__name__)
 
-from .base import BaseCommand
-class PublishCommand(BaseCommand):
+def publishBuilds(cfg: dict) -> int:
+    log.info('Publishing compiled resources...')
+    publish_cfg = cfg.get('publish', {})
+    ytffmpeg_cfg = utils.load()
+    for video_cfg in ytffmpeg_cfg['videos']:
+        output = video_cfg['output']
+        if not output or not os.path.exists(output):
+            log.warning(f'Potentially unbuilt artifact? {output} does not exist!')
+            continue
+        if 'attributes' in video_cfg and 'no-publish' in video_cfg['attributes']:
+            log.info(f'Skipping \x1b[1m{output}\x1b[0m as it is not meant for publishing.')
+            continue
+        log.info(f'Publishing \x1b[1m{output}\x1b[0m...')
 
-    def execute(self) -> int:
-        '''
-        Run the publish command for the given resource.
-        '''
-        if 'publish' not in self.config['ytffmpeg']:
-            log.warning('No publish configuration found in ytffmpeg.yml!')
-            return 1
-        publish_cfg = self.config['ytffmpeg']['publish']
-        log.info('Publishing compiled resources...')
-        if 'resource' in self.config['ytffmpeg']:
-            videos = list( filter(lambda x: x['output'] == self.config['ytffmpeg']['resource'], self.config['videos']) ) or []
-        else:
-            videos = self.config['videos']
-        while videos:
-            video = videos.pop(0)
-            output = video['output']
-            if output and os.path.exists(output):
-                if 'attributes' in video and 'no-publish' in video['attributes']:
-                    log.info(f'Skipping \x1b[1m{output}\x1b[0m as it is not meant for publishing.')
-                    continue
-                log.info(f'Publishing \x1b[1m{output}\x1b[0m...')
+        if 'youtube' in publish_cfg:
+            log.info('**NOT IMPLEMENTED** Would be Publishing to YouTube...')
 
-                if 'youtube' in publish_cfg:
-                    log.info('Publishing to YouTube...')
+        if 'tiktok' in publish_cfg:
+            log.info('**NOT IMPLEMENTED** Would be Publishing to TikTok...')
 
-                if 'tiktok' in publish_cfg:
-                    log.info('Publishing to TikTok...')
-
-                if 'sftp' in publish_cfg:
-                    log.info('Publishing to SFTP Endpoint...')
-                    sftp_host = publish_cfg["sftp"]["host"]
-                    remote_filename = os.path.join(publish_cfg['sftp']['out_dir'], video.get('publish', os.path.basename(output)))
-                    log.info(f'Publishing to {sftp_host} as {remote_filename}')
-                    c = Connection(sftp_host)
-                    c.put(output, remote_filename)
-                    if 'post_publish_cmd' in publish_cfg['sftp']:
-                        post_publish_cmd = publish_cfg['sftp']['post_publish_cmd']
-                        log.info(f'Running post-publish command: {post_publish_cmd}')
-                        # If you plan to run more than one command here, I suggest you create a script and put on the remote first.
-                        # Keep this a 1-liner with maybe arguments.
-                        c.run(post_publish_cmd)
-                log.info(f'Done publishing \x1b[1m{output}\x1b[0m!')
-            else:
-                log.warning(f'Potentially unbuilt artifact? {output} does not exist!')
-        log.info('Complete publishing videos!')
-        return 0
-
-def publisher(config: dict) -> int:
-    cmd = PublishCommand(config)
-    return cmd.execute()
+        if 'sftp' in publish_cfg:
+            log.info('Publishing to SFTP Endpoint...')
+            sftp_host = publish_cfg["sftp"]["host"]
+            remote_filename = os.path.join(publish_cfg['sftp']['out_dir'], video_cfg.get('publish', os.path.basename(output)))
+            log.info(f'Publishing to {sftp_host} as {remote_filename}')
+            c = Connection(sftp_host)
+            c.put(output, remote_filename)
+            if 'post_publish_cmd' in publish_cfg['sftp']:
+                post_publish_cmd = publish_cfg['sftp']['post_publish_cmd']
+                log.info(f'Running post-publish command: {post_publish_cmd}')
+                # If you plan to run more than one command here, I suggest you create a script and put on the remote first.
+                # Keep this a 1-liner with maybe arguments.
+                c.run(post_publish_cmd)
+        log.info(f'Done publishing \x1b[1m{output}\x1b[0m!')
+    log.info('Complete publishing videos!')
+    return 0
